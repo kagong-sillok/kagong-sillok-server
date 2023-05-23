1c6721d43e93f3d7b858fbe988182effc53d8e84
yxxnghwan <younghwan960@gmail.com>
2023-05-23 22:10:55 +0900

    init project

---
+++ src/main/java/org/prography/kagongsillok/KagongsillokApplication.java
@@ -0,0 +1,13 @@
+package org.prography.kagongsillok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KagongsillokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KagongsillokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/prography/kagongsillok/KagongsillokApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prography.kagongsillok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KagongsillokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
