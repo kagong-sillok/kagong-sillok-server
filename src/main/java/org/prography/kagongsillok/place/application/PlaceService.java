560782cdc5ea1ac4a4d29409b9d606bf3d5b60cb
yxxnghwan <younghwan960@gmail.com>
2023-06-05 00:50:45 +0900

    feat(api): 장소 상세 조회 api 개발

--- src/main/java/org/prography/kagongsillok/place/application/PlaceService.java
+++ src/main/java/org/prography/kagongsillok/place/application/PlaceService.java
@@ -3,6 +3,7 @@ package org.prography.kagongsillok.place.application;
 import lombok.RequiredArgsConstructor;
 import org.prography.kagongsillok.place.application.dto.PlaceCreateCommand;
 import org.prography.kagongsillok.place.application.dto.PlaceDto;
+import org.prography.kagongsillok.place.application.exception.NotFoundPlaceException;
 import org.prography.kagongsillok.place.domain.Place;
 import org.prography.kagongsillok.place.domain.PlaceRepository;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,11 @@ public class PlaceService {
     public PlaceDto createPlace(final PlaceCreateCommand placeCreateCommand) {
         final Place savedPlace = placeRepository.save(placeCreateCommand.toEntity());
         return PlaceDto.from(savedPlace);
+    }
+
+    public PlaceDto getPlace(final Long id) {
+        final Place place = placeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundPlaceException(id));
+        return PlaceDto.from(place);
     }
 }
--- src/main/java/org/prography/kagongsillok/place/ui/PlaceV1Controller.java
+++ src/main/java/org/prography/kagongsillok/place/ui/PlaceV1Controller.java
@@ -4,30 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.prography.kagongsillok.common.web.dto.CommonResponse;
 import org.prography.kagongsillok.place.application.PlaceService;
 import org.prography.kagongsillok.place.application.dto.PlaceDto;
-import org.prography.kagongsillok.place.ui.dto.PlaceCreateRequest;
 import org.prography.kagongsillok.place.ui.dto.PlaceResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin/v1/places")
-public class PlaceV1AdminController {
+@RequestMapping("/api/v1/places")
+public class PlaceV1Controller {
 
     private final PlaceService placeService;
-
-    @PostMapping
-    public ResponseEntity<CommonResponse<PlaceResponse>> createPlace(
-            @RequestBody final PlaceCreateRequest placeCreateRequest
-    ) {
-        final PlaceDto createdPlace = placeService.createPlace(placeCreateRequest.toCommand());
-        return CommonResponse.success(PlaceResponse.from(createdPlace));
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<CommonResponse<PlaceResponse>> getPlace(@PathVariable("id") final Long id) {
