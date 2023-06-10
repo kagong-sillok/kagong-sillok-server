22b0ecdbe3f6d277370527f531d001c9d79b1afe
SonMinGyu <57453070+SonMinGyu@users.noreply.github.com>
2023-06-10 14:56:57 +0900

    fix: cors 오류 해결 (#3)
    - cors 오류를 해결함

---
+++ src/main/java/org/prography/kagongsillok/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.prography.kagongsillok.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE = 3600;
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(MAX_AGE);
+    }
+}
