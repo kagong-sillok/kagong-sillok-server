bfa41592f61ffe59dd7b20e60984711d8a02f727
yxxnghwan <younghwan960@gmail.com>
2023-06-23 01:42:08 +0900

    test: BCryptPasswordEncryptor 테스트

---
+++ src/test/java/org/prography/kagongsillok/auth/infrastructure/BCryptPasswordEncryptorTest.java
@@ -0,0 +1,32 @@
+package org.prography.kagongsillok.auth.infrastructure;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+class BCryptPasswordEncryptorTest {
+
+    private final BCryptPasswordEncryptor bCryptPasswordEncryptor
+            = new BCryptPasswordEncryptor(new BCryptPasswordEncoder());
+
+    @Test
+    void 비밀번호를_암호화하고_결과를_비교한다() {
+        final String planePassword = "testPassword123@";
+
+        final String encryptedPassword = bCryptPasswordEncryptor.encrypt(planePassword);
+
+        assertThat(bCryptPasswordEncryptor.matches(planePassword, encryptedPassword)).isTrue();
+    }
+
+    @Test
+    void 안맞는_비밀번호를_비교하면_false_가_반환된다() {
+        final String planePassword = "testPassword123@";
+
+        final String encryptedPassword = bCryptPasswordEncryptor.encrypt(planePassword);
+
+        assertThat(bCryptPasswordEncryptor.matches("notMatchPassword", encryptedPassword)).isFalse();
+    }
+}
