14efa98efbbb9f7fa99c5ffdf9cae81845a8dd8f
yxxnghwan <younghwan960@gmail.com>
2023-06-24 21:25:57 +0900

    test: LocalAccount 테스트코드 작성

---
+++ src/test/java/org/prography/kagongsillok/auth/domain/entity/LocalAccountTest.java
@@ -0,0 +1,37 @@
+package org.prography.kagongsillok.auth.domain.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.Test;
+import org.prography.kagongsillok.member.domain.Member;
+import org.prography.kagongsillok.member.domain.Role;
+
+class LocalAccountTest {
+
+    @Test
+    void 로컬_계정을_생성한다() {
+        final Member member = new Member("테스트", "test@test.com", Role.MEMBER);
+
+        final LocalAccount localAccount = new LocalAccount("loginId", "password", member);
+
+        assertAll(
+                () -> assertThat(localAccount.getLoginId()).isEqualTo("loginId"),
+                () -> assertThat(localAccount.getEncryptedPassword()).isEqualTo("password"),
+                () -> assertThat(localAccount.getMember().getNickname()).isEqualTo("테스트"),
+                () -> assertThat(localAccount.getMember().getEmail()).isEqualTo("test@test.com"),
+                () -> assertThat(localAccount.getMember().getRole()).isSameAs(Role.MEMBER),
+                () -> assertThat(localAccount.getIsDeleted()).isFalse()
+        );
+    }
+
+    @Test
+    void 로컬_계정을_삭제한다() {
+        final Member member = new Member("테스트", "test@test.com", Role.MEMBER);
+        final LocalAccount localAccount = new LocalAccount("loginId", "password", member);
+
+        localAccount.delete();
+
+        assertThat(localAccount.getIsDeleted()).isTrue();
+    }
+}
