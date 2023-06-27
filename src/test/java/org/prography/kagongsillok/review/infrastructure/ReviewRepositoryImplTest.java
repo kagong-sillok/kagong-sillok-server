60e463b954fc5ed12130a8fee2468233086670d3
SonMinGyu <littleking13@naver.com>
2023-06-27 15:34:58 +0900

    test: infrastructure memberId로 review 조회 test 구현

--- src/test/java/org/prography/kagongsillok/review/infrastructure/ReviewRepositoryImplTest.java
+++ src/test/java/org/prography/kagongsillok/review/infrastructure/ReviewRepositoryImplTest.java
@@ -47,5 +47,54 @@ public class ReviewRepositoryImplTest {
         );
     }
 
+    @Test
+    void 멤버_ID로_작성한_리뷰들을_조회한다() {
+        final ReviewCreateCommand reviewCreateCommand1 = ReviewCreateCommand
+                .builder()
+                .rating(4)
+                .memberId(5L)
+                .content("test review1")
+                .imageUrls(List.of("image1", "image2", "image3"))
+                .tags(List.of("#tag1", "#tag2", "#tag3"))
+                .build();
+        final ReviewCreateCommand reviewCreateCommand2 = ReviewCreateCommand
+                .builder()
+                .rating(2)
+                .memberId(5L)
+                .content("test review2")
+                .imageUrls(List.of("image1", "image2", "image3"))
+                .tags(List.of("#tag1", "#tag2", "#tag3"))
+                .build();
+        final ReviewCreateCommand reviewCreateCommand3 = ReviewCreateCommand
+                .builder()
+                .rating(5)
+                .memberId(5L)
+                .content("test review3")
+                .imageUrls(List.of("image1", "image2", "image3"))
+                .tags(List.of("#tag1", "#tag2", "#tag3"))
+                .build();
+        final ReviewCreateCommand reviewCreateCommand4 = ReviewCreateCommand
+                .builder()
+                .rating(1)
+                .memberId(5L)
+                .content("test review4")
+                .imageUrls(List.of("image1", "image2", "image3"))
+                .tags(List.of("#tag1", "#tag2", "#tag3"))
+                .build();
+        reviewRepository.save(reviewCreateCommand1.toEntity());
+        reviewRepository.save(reviewCreateCommand2.toEntity());
+        reviewRepository.save(reviewCreateCommand3.toEntity());
+        reviewRepository.save(reviewCreateCommand4.toEntity());
+
+        final List<Review> reviews = reviewRepository.findAllById(5L);
+
+        assertAll(
+                () -> assertThat(reviews.size()).isEqualTo(4),
+                () -> assertThat(reviews).extracting("memberId").containsAll(List.of(5L, 5L, 5L, 5L)),
+                () -> assertThat(reviews).extracting("rating").containsAll(List.of(4, 2, 5, 1)),
+                () -> assertThat(reviews).extracting("content")
+                        .containsAll(List.of("test review1", "test review2", "test review3", "test review4"))
+        );
+    }
 
 }
