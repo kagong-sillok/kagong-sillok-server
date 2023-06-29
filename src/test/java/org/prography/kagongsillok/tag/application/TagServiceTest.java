00beeedc7b273582fbd5cc130462268d5826b1b7
SonMinGyu <littleking13@naver.com>
2023-06-30 00:59:53 +0900

    test: application tag 조회 API 구현

--- src/test/java/org/prography/kagongsillok/tag/application/TagServiceTest.java
+++ src/test/java/org/prography/kagongsillok/tag/application/TagServiceTest.java
@@ -3,6 +3,7 @@ package org.prography.kagongsillok.tag.application;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.prography.kagongsillok.tag.application.dto.TagCreateCommand;
 import org.prography.kagongsillok.tag.application.dto.TagDto;
@@ -26,6 +27,44 @@ public class TagServiceTest {
         assertAll(
                 () -> assertThat(createdTag.getTagName()).isEqualTo("#tag1"),
                 () -> assertThat(createdTag.getTagContent()).isEqualTo("test tag")
+        );
+    }
+
+    @Test
+    void 태그_ID로_태그를_조회한다() {
+        final TagCreateCommand tagCreateCommand1 = new TagCreateCommand("#tag1", "test tag1");
+        final TagCreateCommand tagCreateCommand2 = new TagCreateCommand("#tag2", "test tag2");
+        final TagCreateCommand tagCreateCommand3 = new TagCreateCommand("#tag3", "test tag3");
+        final Long createdTag1_id = tagService.createTag(tagCreateCommand1).getId();
+        final Long createdTag2_id = tagService.createTag(tagCreateCommand2).getId();
+        final Long createdTag3_id = tagService.createTag(tagCreateCommand3).getId();
+
+        final List<TagDto> tagDtos = tagService.getTags(List.of(createdTag1_id, createdTag3_id));
+
+        assertAll(
+                () -> assertThat(tagDtos).extracting("tagName")
+                        .containsAll(List.of("#tag1", "#tag3")),
+                () -> assertThat(tagDtos).extracting("tagContent")
+                        .containsAll(List.of("test tag1", "test tag3"))
+        );
+    }
+
+    @Test
+    void 모든_태그를_조회한다() {
+        final TagCreateCommand tagCreateCommand1 = new TagCreateCommand("#tag1", "test tag1");
+        final TagCreateCommand tagCreateCommand2 = new TagCreateCommand("#tag2", "test tag2");
+        final TagCreateCommand tagCreateCommand3 = new TagCreateCommand("#tag3", "test tag3");
+        tagService.createTag(tagCreateCommand1).getId();
+        tagService.createTag(tagCreateCommand2).getId();
+        tagService.createTag(tagCreateCommand3).getId();
+
+        final List<TagDto> tagDtos = tagService.getAllTags();
+
+        assertAll(
+                () -> assertThat(tagDtos).extracting("tagName")
+                        .containsAll(List.of("#tag1", "#tag2", "#tag3")),
+                () -> assertThat(tagDtos).extracting("tagContent")
+                        .containsAll(List.of("test tag1", "test tag2", "test tag3"))
         );
     }
 }
