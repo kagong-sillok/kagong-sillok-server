6ff510a94a10f26f313ff6d346ba7e43d275a330
SonMinGyu <littleking13@naver.com>
2023-06-29 17:00:57 +0900

    test: review 생성 테스트 수정

--- src/test/java/org/prography/kagongsillok/review/domain/ReviewTest.java
+++ src/test/java/org/prography/kagongsillok/review/domain/ReviewTest.java
@@ -3,28 +3,59 @@ package org.prography.kagongsillok.review.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
+import org.prography.kagongsillok.ReviewTag.domain.ReviewTag;
+import org.prography.kagongsillok.ReviewTag.domain.ReviewTags;
+import org.prography.kagongsillok.tag.domain.Tag;
 
 public class ReviewTest {
 
-//    @Test
-//    void 리뷰를_생성한다() {
-//        final Review review = Review
-//                .builder()
-//                .rating(4)
-//                .content("test review")
-//                .tags(Tags.of(Set.of("#tag1", "#tag2")))
-//                .images(Images.of(Set.of("image1", "image2")))
-//                .memberId(1L)
-//                .build();
-//
-//        assertAll(
-//                () -> assertThat(review.getRating()).isEqualTo(4),
-//                () -> assertThat(review.getContent()).isEqualTo("test review"),
-//                () -> assertThat(review.getTags().getTags()).containsAll(Set.of("#tag1", "#tag2")),
-//                () -> assertThat(review.getImages().getImages()).containsAll(Set.of("image1", "image2")),
-//                () -> assertThat(review.getMemberId()).isEqualTo(1L)
-//        );
-//    }
+    @Test
+    void 태그가_없는_리뷰를_생성한다() {
+        final Review review = Review
+                .builder()
+                .rating(4)
+                .content("test review")
+                .imageIds(List.of(1L, 2L, 3L))
+                .tags(ReviewTags.of(new ArrayList<>()))
+                .memberId(1L)
+                .build();
+
+        assertAll(
+                () -> assertThat(review.getRating()).isEqualTo(4),
+                () -> assertThat(review.getContent()).isEqualTo("test review"),
+                () -> assertThat(review.getImageIds()).isEqualTo(List.of(1L, 2L, 3L)),
+                () -> assertThat(review.getTags().getReviewTags()).isEqualTo(new ArrayList<>()),
+                () -> assertThat(review.getMemberId()).isEqualTo(1L)
+        );
+    }
+
+    @Test
+    void 태그가_있는_리뷰를_생성한다() {
+        final Tag tag1 = new Tag("#tag1", "test tag1");
+        final Tag tag2 = new Tag("#tag2", "test tag2");
+        final Review review = Review
+                .builder()
+                .rating(4)
+                .content("test review")
+                .imageIds(List.of(1L, 2L, 3L))
+                .tags(ReviewTags.of(new ArrayList<>()))
+                .memberId(1L)
+                .build();
+        final ReviewTag reviewTag1 = new ReviewTag(review, tag1);
+        final ReviewTag reviewTag2 = new ReviewTag(review, tag2);
+
+        assertAll(
+                () -> assertThat(review.getRating()).isEqualTo(4),
+                () -> assertThat(review.getContent()).isEqualTo("test review"),
+                () -> assertThat(review.getImageIds()).isEqualTo(List.of(1L, 2L, 3L)),
+                () -> assertThat(review.getTags().getReviewTags().size()).isEqualTo(2),
+                () -> assertThat(review.getTags().getReviewTags()).extracting("tag")
+                        .extracting("tagName").containsAll(List.of("#tag1", "#tag2")),
+                () -> assertThat(review.getMemberId()).isEqualTo(1L)
+        );
+    }
 }
