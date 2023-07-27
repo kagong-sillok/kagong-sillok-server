4a40b96238a925f5bf5c1296d8bd04afb923c75d
SonMinGyu <littleking13@naver.com>
2023-07-27 17:49:36 +0900

    refactor: Member 조회시 accessToken에서 id 찾도록 리팩토링
    - Member 조회시 accessToken에서 id 값을 가져오도록 리팩토링함

--- src/main/java/org/prography/kagongsillok/member/application/MemberService.java
+++ src/main/java/org/prography/kagongsillok/member/application/MemberService.java
@@ -2,6 +2,7 @@ package org.prography.kagongsillok.member.application;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.prography.kagongsillok.auth.infrastructure.JwtAuthTokenProvider;
 import org.prography.kagongsillok.member.application.dto.MemberDto;
 import org.prography.kagongsillok.member.application.exception.NotFoundMemberException;
 import org.prography.kagongsillok.member.domain.Member;
@@ -18,15 +19,17 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final StudyRecordRepository studyRecordRepository;
+    private final JwtAuthTokenProvider jwtAuthTokenProvider;
 
-    public MemberDto getMember(final Long id) {
-        final Member member = memberRepository.findById(id)
-                .orElseThrow(() -> new NotFoundMemberException(id));
+    public MemberDto getMember(final String accessToken) {
+        Long memberId = jwtAuthTokenProvider.getLoginMemberByAccessToken(accessToken).getMemberId();
+        final Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundMemberException(memberId));
         if (member.getIsDeleted()) {
-            throw new NotFoundMemberException(id);
+            throw new NotFoundMemberException(memberId);
         }
 
-        List<StudyRecord> memberStudyRecords = studyRecordRepository.findMemberRecordByMemberId(id);
+        List<StudyRecord> memberStudyRecords = studyRecordRepository.findMemberRecordByMemberId(memberId);
 
         return MemberDto.from(member, calculateTotalStudyTime(memberStudyRecords));
     }
--- src/main/java/org/prography/kagongsillok/member/ui/MemberV1Controller.java
+++ src/main/java/org/prography/kagongsillok/member/ui/MemberV1Controller.java
@@ -1,5 +1,6 @@
 package org.prography.kagongsillok.member.ui;
 
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.prography.kagongsillok.common.web.dto.CommonResponse;
 import org.prography.kagongsillok.member.application.MemberService;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,10 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberV1Controller {
 
     private final MemberService memberService;
-
-    @GetMapping("/{id}")
-    public ResponseEntity<CommonResponse<MemberResponse>> getMember(@PathVariable("id") final Long id) {
-        final MemberDto memberDto = memberService.getMember(id);
+    
+    @GetMapping
+    public ResponseEntity<CommonResponse<MemberResponse>> getMember(
+            HttpServletRequest request
+    ) {
+        String accessToken = getAccessToken(request.getHeader("accessToken"));
+        final MemberDto memberDto = memberService.getMember(accessToken);
         return CommonResponse.success(MemberResponse.from(memberDto));
+    }
+
+    private String getAccessToken(String accessTokenHeader) {
+        return accessTokenHeader.trim().split(" ")[1];
     }
 }
